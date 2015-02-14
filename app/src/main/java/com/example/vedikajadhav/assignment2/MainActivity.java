3d031434ec9ead2ad5e27832d606b0d41dc0a390
Wedika Jadhav <vedikajadhav48@gmail.com>
2015-02-14 13:18:27 -0800

    TAG = MainActivity

--- app/src/main/java/com/example/vedikajadhav/assignment2/MainActivity.java
+++ app/src/main/java/com/example/vedikajadhav/assignment2/MainActivity.java
@@ -8,6 +8,8 @@ import android.view.MenuItem;
 
 public class MainActivity extends ActionBarActivity {
 
+    private static final String TAG = "MainActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
