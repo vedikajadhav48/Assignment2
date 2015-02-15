ac692a517d3bf98efaa401cc562d03d77c13361e
Wedika Jadhav <vedikajadhav48@gmail.com>
2015-02-15 03:32:16 -0800

    TAG = MainActivity

---
+++ app/src/main/java/com/example/vedikajadhav/assignment2/DesertListFragment.java
@@ -0,0 +1,15 @@
+package com.example.vedikajadhav.assignment2;
+
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+
+/**
+ * Created by Vedika Jadhav on 2/15/2015.
+ */
+public class DesertListFragment extends ListFragment{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+    }
+}
